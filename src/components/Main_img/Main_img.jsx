13434e7ef0885dbb1623bf9695bca6375858d3cf
raggedband13 <137692302+raggedband13@users.noreply.github.com>
2024-07-03 13:58:32 +0300

    Delete src directory

--- src/components/Main_img/Main_img.jsx
+++
@@ -1,9 +0,0 @@
-import classes from './Main_img.module.css';
-
-function Main_img( {src} ) {
-    return (
-        <img className={classes.main_img} src={src} />
-    )
-}
-
-export default Main_img;
